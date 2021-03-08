a51072d39cba993089a2be81516fe5c85e0cedd1
badhon1512 <badhon1512@gmail.com>
2021-03-08 20:29:06 +0600

    some changes to data

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
 
       
       <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Tech-Hub</Navbar.Brand>
+    <Navbar.Brand href="/">Tech-Hub</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Laptop</Nav.Link>
